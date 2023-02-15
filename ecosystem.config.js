9941ab10881fb9ba137cac7ac3db70e535598573
andre <andreluiz.jpofi@gmail.com>
2023-02-15 19:20:07 -0300

    update ecosystem.config 2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,10 @@
 const path = require('path')
 
-require('path')
-
 module.exports = {
   apps: [
     {
       name: 'app',
       script: path.resolve(__dirname, 'src', 'server.js'),
-      instances: 'max',
       env: {
         NODE_ENV: 'development'
       },
@@ -17,3 +14,21 @@ module.exports = {
     }
   ]
 }
+
+// const path = require('path')
+
+// module.exports = {
+//   apps: [
+//     {
+//       name: 'app',
+//       script: path.resolve(__dirname, 'src', 'server.js'),
+//       instances: 'max',
+//       env: {
+//         NODE_ENV: 'development'
+//       },
+//       env_production: {
+//         NODE_ENV: 'production'
+//       }
+//     }
+//   ]
+// }
