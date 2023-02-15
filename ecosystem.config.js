e16959c08b355c3d9e0b1739fcdbb7337c0d26f6
andre <andreluiz.jpofi@gmail.com>
2023-02-15 18:28:10 -0300

    ecosystem.config update path server.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,19 @@
+const path = require('path')
+
+require('path')
+
 module.exports = {
-  apps : [{
-    name: "app",
-    script: "./src/server.js",
-    instances: "max",
-    env: {
-      NODE_ENV: "development",
-    },
-    env_production: {
-      NODE_ENV: "production",
+  apps: [
+    {
+      name: 'app',
+      script: path.resolve(__dirname, 'src', 'server.js'),
+      instances: 'max',
+      env: {
+        NODE_ENV: 'development'
+      },
+      env_production: {
+        NODE_ENV: 'production'
+      }
     }
-  }]
+  ]
 }
